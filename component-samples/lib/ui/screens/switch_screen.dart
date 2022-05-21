08772afb604a67d67390a14c7d01357f7f563961
lv-kit <taka.mc.0509@gmail.com>
2022-05-21 13:14:58 +0900

    Update switch_screen.dart

--- component-samples/lib/ui/screens/switch_screen.dart
+++ component-samples/lib/ui/screens/switch_screen.dart
@@ -8,11 +8,32 @@ class SwitchScreen extends StatefulWidget {
 }
 
 class _SwitchScreen extends State<SwitchScreen> {
+  bool _createSampleValue1 = false;
+  final bool _createSampleValue2 = true;
+
   @override
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("home"),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text("value1"),
+            trailing: Switch(
+              value: _createSampleValue1,
+              onChanged: (value) => setState(() => _createSampleValue1 = value),
+            ),
+          ),
+          ListTile(
+            title: const Text("value2"),
+            trailing: Switch(
+              value: _createSampleValue2,
+              onChanged: null,
+            ),
+          ),
+        ],
       ),
     );
   }
